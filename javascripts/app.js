35769ded1cc547dd5c0cd2a8bcb3840dcf66f9f4
asimonia <alex.simonian@gmail.com>
2016-11-15 22:12:40 -0600

    Boilerplate html&css

--- javascripts/app.js
+++ javascripts/app.js
@@ -5,3 +5,5 @@
 // Provide a Attack! button that, when clicked, simply applies the damage output of each robot against the other one.
 // Once either robot's health is <0 display a message that the battle is over, and which one won. For example...
 // The Viper Drone defeated the Behemoth ATV with its flamethrower.
+
+$("#player-setup").show();
